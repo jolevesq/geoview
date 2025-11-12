eac83752d38b66de96d75e8a04e4b41a9c35623c
Johann Levesque <johann.levesque@nrcan-rncan.gc.ca>
2025-11-12 14:00:56 -0500

    Updates

--- workers/json-export-worker-script.worker.js
+++ workers/json-export-worker-script.worker.js
@@ -1,4 +1,4 @@
-/*! Package:json-export-worker-script: 2.0.2 - "7064dcfbe2b6c3f94ee8248c071b41b0197f8a9c" - 2025-11-12T14:07:59.733Z */(()=>{"use strict";
+/*! Package:json-export-worker-script: 2.0.2 - "1bec338b84cd6a5a3099cbde1ddb021ccdfbdaaa" - 2025-11-12T18:48:31.527Z */(()=>{"use strict";
 /**
  * @license
  * Copyright 2019 Google LLC
