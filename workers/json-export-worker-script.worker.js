c5868ad4569d5795fcf78c9fd799e70748163cc1
Johann Levesque <johann.levesque@canada.ca>
2026-02-27 10:03:58 -0500

    Updates

--- workers/json-export-worker-script.worker.js
+++ workers/json-export-worker-script.worker.js
@@ -1,4 +1,4 @@
-/*! Package:json-export-worker-script: 2.1.2 - "777e2a1f9b0f3509a9f2f6c483d53298dfe1ed23" - 2026-02-26T19:47:26.990Z */(()=>{"use strict";
+/*! Package:json-export-worker-script: 2.1.2 - "2f8c9cbd393719f04869b4e6fd56dc8b64b0c3d1" - 2026-02-27T14:39:13.508Z */(()=>{"use strict";
 /**
  * @license
  * Copyright 2019 Google LLC
